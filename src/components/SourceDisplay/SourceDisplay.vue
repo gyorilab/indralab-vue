099f005aab469da2c74a46190594acd75b2e1e66
kkaris <karis.klas@gmail.com>
2021-05-21 15:32:12 -0400

    Hardcode sources

--- src/components/SourceDisplay/SourceDisplay.vue
+++ src/components/SourceDisplay/SourceDisplay.vue
@@ -1,7 +1,10 @@
 <template>
   <span class="source-display">
     <span v-for="(src_group, cat, idx) in sources" :key="cat">
-      <span v-if="idx > 0" class="badge badge-source">|</span>
+      <span
+          v-if="idx > 0"
+          style="color: #212529;"
+          class="badge badge-source">|</span>
       <span v-for="src in src_group"
             :key="src">
         <span :class="`${badgeClass} source-${src}`"
@@ -39,11 +42,44 @@
         return false;
       },
     },
+    data() {
+      return {
+        sources: {
+          'databases': [
+              'psp',
+              'cbn',
+              'pc',
+              'bel_lc',
+              'signor',
+              'biogrid',
+              'lincs_drug',
+              'tas',
+              'hprd',
+              'trrust',
+              'ctd',
+              'vhn',
+              'pe',
+              'drugbank',
+              'omnipath',
+              'conib',
+              'crog',
+              'dgi'
+          ],
+          'reading': [
+              'rlimsp',
+              'isi',
+              'tees',
+              'geneways',
+              'eidos',
+              'trips',
+              'medscan',
+              'sparser',
+              'reach'
+          ]
+        }
+      }
+    },
     computed: {
-      sources: function() {
-        return this.$sources
-      },
-
       badgeClass: function() {
         let base = 'badge badge-source';
         if (this.source_counts === null)
